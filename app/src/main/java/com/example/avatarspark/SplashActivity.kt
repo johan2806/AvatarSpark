1d9a58b26f9972962867660d489811d53b9c3790
johan.vasquez <johan.vasquez@aossas.com>
2025-09-03 21:36:45 -0500

    Second Commit

---
+++ app/src/main/java/com/example/avatarspark/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.avatarspark
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() //
+        }, 2000)
+    }
+}
